7e60de7762caee6a75241a2715438f037bd97429
Indresh <ips.indresh@gmail.com>
2018-03-26 09:27:24 +0530

    interpolation and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  description = "this is angular application";
+
+  colors = ["red", "green", "blue"];
+
+  user = { username: "test username" };
+  flag = true;
+
+  ref = "row_id";
+  cls;
+
+  state_cls;
+  constructor() {
+    setTimeout(() => {
+      this.description = "description modified";
+      this.colors = [...this.colors,"White"];
+      console.log(this.colors);
+      this.flag = false;
+      this.user.username = "Indresh";
+      this.ref="changed_id";
+      this.cls = "box padding";
+      this.state_cls="success ";
+    }, 5000);
+  }
 }
