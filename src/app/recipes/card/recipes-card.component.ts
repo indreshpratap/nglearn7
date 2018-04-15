9ac2e2ed8807e28ff0f166fd68814c2c735d28b1
Indresh <ips.indresh@gmail.com>
2018-04-15 14:30:22 +0530

    @output example

--- src/app/recipes/card/recipes-card.component.ts
+++ src/app/recipes/card/recipes-card.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-recipes-card",
   templateUrl: "./recipes-card.component.html"
 })
 export class RecipesCardComponent {
-    @Input()
-    recipe:any;
-    constructor(){
-        this.recipe = {};
-    }
+  @Input() recipe: any;
+  @Input() offer: string;
+  @Input("bgcolor") background_color:string;
+  @Input() type:number;
+  @Output() onAddToFab:EventEmitter<any> = new EventEmitter<any>();
+  @Output() onRemoveFromFab:EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {
+    this.recipe = {};
+  }
+
+  handleFavBtn(){
+      console.log("Yes Fab clicked");
+      this.onAddToFab.emit(this.recipe);
+  }
+  handleRemoveBtn(){
+    console.log("Yes remove clicked");
+    this.onRemoveFromFab.emit(this.recipe);
+}
 }
--- src/app/recipes/listing/recipes.listing.component.ts
+++ src/app/recipes/listing/recipes.listing.component.ts
@@ -31,12 +31,23 @@ export class RecipesListingComponent {
     ];
 
     this.favRecipes = [
-        {
-            title: "Recipes 3",
-            description: " lsfskdjfl  lfdskj l",
-            image: "http://via.placeholder.com/350x150",
-            ingredients: ["Item 1", "Item 2", "Item 3"]
-          }
-    ]
+      {
+        title: "Recipes 3",
+        description: " lsfskdjfl  lfdskj l",
+        image: "http://via.placeholder.com/350x150",
+        ingredients: ["Item 1", "Item 2", "Item 3"]
+      }
+    ];
+  }
+
+  addToFab(data) {
+    console.log("Received request for ", data);
+    this.favRecipes.push(data);
+  }
+  removeFromFab(item) {
+    let indx = this.favRecipes.indexOf(item);
+    if (indx > -1) {
+      this.favRecipes.splice(indx, 1);
+    }
   }
 }
